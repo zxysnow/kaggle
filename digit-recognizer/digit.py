52470a6676d499a80b9f6334bf167b5a6ab528b3
zxysnow <zxy_snow@qq.com>
2015-03-24 21:59:37 +0800

    knn with pca

--- digit-recognizer/digit.py
+++ digit-recognizer/digit.py
@@ -1,18 +1,23 @@
 import sys
 import csv
-import numpy
+from numpy import *
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import decomposition
 
-def loadTrainData() :
+def loadTrainData(file_name) :
 	l = []
-	with open('train.csv') as file :
+	header = False
+	with open(file_name) as file :
 		lines = csv.reader(file)
 		for line in lines :
-			l.append(line)
-	l.remove(l[0])
+			if header == False :
+				header = True
+				continue
+			l.append([int(i) for i in line])
 	l = array(l)
+	train = l[:, 1:]
 	label = l[:, 0]
-	data = l[:, 1:]
-	return nomalizing(toInt(data)), toInt(label)
+	return normalizatio(train), label.tolist()
 
 def toInt(array) :
 	array = mat(array)
@@ -23,7 +28,8 @@ def toInt(array) :
 			newArray[i, j] = int(array[i, j])
 	return newArray
 
-def nomalizing(array) :
+def normalization(array) :
+#	return array
 	m, n = shape(array)
 	for i in xrange(m) :
 		for j in xrange(n) :
@@ -31,35 +37,74 @@ def nomalizing(array) :
 				array[i, j] = 1
 	return array
 
-def loadTestData() :
+def loadTestData(file_name) :
 	l = []
-	with open('test.csv') as file :
+	with open(file_name) as file :
 		lines = csv.reader(file)
 		for line in lines :
 			l.append(line)
 	l.remove(l[0])
 	data = array(l)
-	return nomalizing(toInt(data))
+	return normalization(toInt(data))
 
-
-def loadTestResult() :
+def loadTestResult(file_name) :
 	l = []
-	with open('knn_benchmark.csv') as file :
+	header = False
+	with open(file_name) as file :
 		lines = csv.reader(file)
 		for line in lines :
-			l.append(line)
-	l.remove(l[0])
-	label = array(l)
-	return toInt(label[:, 1])
+			if header == False :
+				header = True
+				continue
+			l.append(int(line[1]))
+	return l 
 
-def classify(inX, dataSet, labels, k) :
-	inX = mat(inX)
-	oo
+def compare(Y1, Y2) :
+	print 'compare ...'
+	wrong_cnt = 0
+	for i in range(0, len(Y1)) :
+		if Y1[i] != Y2[i] :
+			wrong_cnt += 1
+	print 'wrong rate : '
+	return wrong_cnt * 1.0 / len(Y1)
+
+def classify(train_data, labels, test_data) :
+	train_data = mat(train_data)
+	pca = decomposition.PCA(n_components = 100).fit(train_data)
+#train_reduced = train_data
+	train_reduced = pca.transform(train_data)
+	print "knn--------------------------------"
+	knn = KNeighborsClassifier(n_neighbors = 10, algorithm = "kd_tree")
+	print "knn training..."
+	knn.fit(train_reduced, labels)
+	print 'knn predicting...'
+	test_reduced = test_data
+	test_reduced = pca.transform(test_data)
+	predictions = knn.predict(test_reduced);
+	print 'knn done---------------------------'
+	return predictions
 
 def saveResult(result) :
 	with open('result.csv', 'wb') as myFile :
 		myWriter = csv.writer(myFile)
+		ind = 1
 		for i in result :
-			tmp = []
-			tmp.append(i)
-			myWriter.writerow(tmp)
+			data = [ind, i]
+			myWriter.writerow(data)
+			ind += 1
+
+def main() :
+	print 'load train data...'
+	train_data, labels = loadTrainData('train.csv.bak')
+	print 'load test data...'
+	test_data = loadTestData('test.csv.bak')
+	predictions = classify(train_data, labels, test_data)
+	saveResult(predictions)
+	test_labels = loadTestResult('knn_benchmark.csv.bak')
+	wrong_rate_knn = compare(test_labels, predictions)
+	print wrong_rate_knn
+
+
+if __name__ == '__main__' :
+	main()
+	
